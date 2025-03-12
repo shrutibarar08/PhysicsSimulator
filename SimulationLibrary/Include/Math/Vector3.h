1f346f42ec539acc9c6e53886dabaf7437cf7de3
shrutibarar08 <S.BARAR-2023@gmail.com>
2025-03-12 21:31:56 +0530

    Implement Vector3D class

--- SimulationLibrary/Include/Math/Vector3.h
+++ SimulationLibrary/Include/Math/Vector3.h
@@ -9,12 +9,34 @@ namespace Physics
     public:
         Vector3D();
         Vector3D(const float x, const float y, const float z);
-        
         ~Vector3D() = default;
-
-        
         
         void Invert();
+        void Normalize();
+        void AddScaledVector(const Vector3D& vector, float scale);
+
+        float Magnitude() const;
+        float SquareMagnitude() const;
+
+        void operator*=(const float value);
+        Vector3D operator*(const float value) const;
+
+        void operator+=(const Vector3D& vector);
+        Vector3D operator+(const Vector3D& vector) const;
+
+        void operator-=(const Vector3D& vector);
+        Vector3D operator-(const Vector3D& vector) const;
+
+        Vector3D ComponentProduct(const Vector3D& vector) const;
+        void ComponentProductUpdate(const Vector3D& vector);
+
+        float ScalarProduct(const Vector3D& vector) const;
+        float operator*(const Vector3D& vector) const;
+
+        Vector3D VectorProduct(const Vector3D& vector) const;
+        
+        void operator%=(const Vector3D& vector);    // Update Vector to be a vector product of current values
+        Vector3D operator%(const Vector3D& vector) const; // Return vector product
 
     public:
         float x, y, z;
@@ -22,6 +44,5 @@ namespace Physics
     private:
         float pad;
     };
-
 }
 
